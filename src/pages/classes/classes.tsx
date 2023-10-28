34e85940127a26eb8cd8ef9aa0d82e19c5cbd06d
arthur-kamau <kamaukenn11@gmail.com>
2023-10-29 02:20:17 +0300

    classes page

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -1,10 +1,27 @@
 import type {Component} from 'solid-js';
-
+import NavBar from "../common/navbar";
+import "../../assets/classes.css";
 
 const Classes: Component = () => {
     return (
-        <div>
-            Classes
+        <div  class="shop-container" >
+            <NavBar/>
+
+            <div  class="hero" ></div>
+            <div  class="backdrop">
+
+                <div  class="container h-100">
+                    <div  class="row h-100 w-100 ">
+                        <div  class="col-12 d-flex justify-content-center align-items-center">
+
+                            <div  class="row w-auto " >
+                                <div  class="col-lg-6 text-white h2 text-uppercase  py-2 border-1">Ong-Going</div>
+                                <div  class="col-lg-6 text-white h2 text-uppercase py-2">New</div>
+                            </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
