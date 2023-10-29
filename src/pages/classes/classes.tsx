737cc9eaa3b09ee8981fb1cd0ecc0b8938d4c2b1
Kibuku <gkamau365@gmail.com>
2023-10-29 03:11:00 +0300

    Home Page

--- src/pages/classes/classes.tsx
+++ src/pages/classes/classes.tsx
@@ -15,7 +15,7 @@ const Classes: Component = () => {
                         <div  class="col-12 d-flex justify-content-center align-items-center">
 
                             <div  class="row w-auto " >
-                                <div  class="col-lg-6 text-white h2 text-uppercase  py-2 border-1">Ong-Going</div>
+                                <div  class="col-lg-6 text-white h2 text-uppercase  py-2 border-1">On-Going</div>
                                 <div  class="col-lg-6 text-white h2 text-uppercase py-2">New</div>
                             </div>
                             </div>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,9 +4,29 @@ import styles from '../../assets/App.module.css';
 import NavBar from "../common/navbar";
 const Home: Component = () => {
     return (
-       <div>
-           home
-       </div>
+        <div  >
+        <NavBar/>
+        <div  class="hero2" >
+            <div>
+                <div class="container h-100">
+                    <div class="row h-100">
+                        <div >
+
+                            <div class="row">
+                                <div class="col-lg-4 text-white h2 text-uppercase  py-2 border-1">SHOP</div>
+                                <div class="col-lg-4 text-white h2 text-uppercase py-2">CLASSES</div>
+                                <div class="col-lg-4 text-white h2 text-uppercase py-2">ABOUT US</div>
+                                <div class="col-lg-4 text-white h2 text-uppercase " >CONTACT US</div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+               
+            </div>
+        </div>
+        </div>
     );
 };
 
