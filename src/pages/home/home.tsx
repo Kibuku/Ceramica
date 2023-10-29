23ddd28575d55d422eeaf33c3e624ded3b216d75
arthur-kamau <kamaukenn11@gmail.com>
2023-10-29 14:47:38 +0300

    more logic

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,14 @@
 import type {Component} from 'solid-js';
 
-import  '../../assets/home.css';
-import NavBar from "../common/navbar";
-import {A} from "@solidjs/router";
+import '../../assets/home.css';
+
+import {A, useNavigate} from "@solidjs/router";
+
 const Home: Component = () => {
+    const navigate = useNavigate();
     return (
-        <div  >
-        {/*<NavBar/>*/}
+        <div>
+            {/*<NavBar/>*/}
             <header class="site-navbar mt-3">
                 <div class="container-fluid">
                     <div class="row align-items-center">
@@ -17,32 +19,36 @@ const Home: Component = () => {
                     </div>
                 </div>
             </header>
-        <div  class="home-hero" >
+            <div class="home-hero">
 
                 <div class="container h-100 ">
                     <div class="h-100  w-100 d-flex justify-content-end align-items-center ">
 
 
+                        <div class="row home-container1 ">
+                            <div
+                                class=" col-lg-5 text-white h3 text-uppercase home-border  rounded-3 d-flex align-items-center justify-content-center" onClick={(e)=>{
+                                    e.preventDefault();
+                                    navigate("/shop");
+                            }}>SHOP
+                            </div>
+                            <div class="col-lg-1 mx-1"></div>
+                            <div class="col-lg-5 text-white h3 text-uppercase py-2 home-border d-flex align-items-center justify-content-center" onClick={(e)=>{
+                                navigate("/classes");
+                            }}>CLASSES</div>
 
-                              <div class="row home-container1 " >
-                                  <div  class=" col-lg-5 text-white h3 text-uppercase home-border  rounded-3 text-center">SHOP</div>
-                                  <div class="col-lg-1 mx-1"></div>
-                                  <div class="col-lg-5 text-white h3 text-uppercase py-2 home-border">CLASSES</div>
-
-<div class="col-lg-12 my-3"></div>
-                                  <div class="col-lg-5 text-white h3 text-uppercase py-4 home-border">ABOUT US</div>
-                                  <div class="col-lg-1 mx-1"></div>
-                                  <div class="col-lg-5 text-white h3 text-uppercase py-4 home-border" >CONTACT US</div>
-                              </div>
-                                
-
-
+                            <div class="col-lg-12 my-3"></div>
+                            <div class="col-lg-5 text-white h4 text-uppercase py-4 home-border d-flex align-items-center justify-content-center"  onClick={(e)=>{
+                                navigate("/about_us");
+                            }}>ABOUT US</div>
+                            <div class="col-lg-1 mx-1"></div>
+                            <div class="col-lg-5 text-white h4 text-uppercase py-4 home-border d-flex align-items-center justify-content-center"  onClick={(e)=>{
+                                navigate("/contact_us");
+                            }}>CONTACT US</div>
+                        </div>
                     </div>
                 </div>
-                
-               
-
-        </div>
+            </div>
         </div>
     );
 };
