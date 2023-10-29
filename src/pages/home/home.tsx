0d11be5db99835bb3a1130a35224a97bd24ae31a
Kibuku <gkamau365@gmail.com>
2023-10-29 12:55:21 +0300

    Home pages

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -6,17 +6,20 @@ const Home: Component = () => {
     return (
         <div  >
         <NavBar/>
+        
         <div  class="home-hero" >
             <div>
                 <div class="container h-100">
                     <div class="row h-100">
                         <div >
 
-                            <div class="row">
+                            <div class="home-container">
                                 <div class="col-lg-4 text-white h2 text-uppercase  py-2 border-1">SHOP</div>
                                 <div class="col-lg-4 text-white h2 text-uppercase py-2">CLASSES</div>
-                                <div class="col-lg-4 text-white h2 text-uppercase py-2">ABOUT US</div>
-                                <div class="col-lg-4 text-white h2 text-uppercase " >CONTACT US</div>
+                            </div>
+                            <div class="home-container">
+                                <div class="col-lg-4 text-white h2 text-uppercase py-1">ABOUT US</div>
+                                <div class="col-lg-4 text-white h2 text-uppercase py-1" >CONTACT US</div>
                                 
                             </div>
                         </div>
